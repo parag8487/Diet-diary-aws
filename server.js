d4769f07f918856ca708a1fcd3b47eedd923ff86
Parag Mohare <paragmohare049@gmail.com>
2025-06-19 01:27:29 +0530

    Cleaned up head sections and removed unnecessary code

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ app.use(bodyParser.json());
 
 //  MySQL Configuration 
 const db = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQL_PORT
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 });
 
 db.connect((err) => {
